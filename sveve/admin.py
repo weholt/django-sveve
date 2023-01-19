7cf8ded08be55b2db92a5ea5a6b1151c52cebd5f
Thomas Weholt <2002@weholt.org>
2023-01-19 23:42:29 +0100

    Sending sms to contacts and groups working.

--- sveve/admin.py
+++ sveve/admin.py
@@ -1,5 +1,9 @@
+from django import forms
 from django.conf import settings
 from django.contrib import admin
+from django.db import models
+from django.shortcuts import redirect, render
+from django.urls import path, reverse
 
 from .models import Contact, Group, Message
 from .services import send_message
@@ -7,20 +11,77 @@ from .services import send_message
 # from django.utils.html import mark_safe
 
 
+class CsvImportForm(forms.Form):
+    csv_file = forms.FileField()
+
+
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ["last_name", "first_name", "mobile_phone"]
+    change_list_template = "admin/contact_changelist.html"
     actions = ["send_message"]
 
     @admin.action(description="Sends a message to the selected contacts")
     def send_message(self, request, queryset):
-        msg = send_message(request.user, settings.SVEVE_SENDER_TEXT, "Sms virker gitt.", queryset)
-        print(msg)
+        if "apply" in request.POST:  # or 'action' in request.POST:
+            message_text = request.POST.get("message").strip()
+            if message_text:
+                msg = send_message(request.user, settings.SVEVE_SENDER_TEXT, message_text, queryset)
+                if msg.status == "sent":
+                    self.message_user(request, "Sent message to {} contacts".format(queryset.count()))
+                else:
+                    self.message_user(request, "Failed to send messag: %s" % msg.status_message)
+                return redirect(reverse("admin:sveve_contact_changelist"))
+
+        return render(request, "admin/send_message.html", context={"orders": queryset})
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path("import-csv/", self.import_csv),
+        ]
+        return my_urls + urls
+
+    def import_csv(self, request):
+        if request.method == "POST":
+            for line in request.FILES["csv_file"].read().decode("utf-8").split("\n"):
+                try:
+                    last_name, first_name, mobile_phone = line.split(",")
+                    Contact.objects.update_or_create(first_name=first_name, last_name=last_name, defaults={"mobile_phone": mobile_phone})
+                except Exception as ex:
+                    print("Error parsing '%s': %s" % (line, ex))
+            self.message_user(request, "Your csv file has been imported")
+            return redirect(reverse("admin:sveve_contact_changelist"))
+        form = CsvImportForm()
+        payload = {"form": form}
+        return render(request, "admin/csv_form.html", payload)
 
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ["name"]
+    formfield_overrides = {
+        models.ManyToManyField: {"widget": forms.CheckboxSelectMultiple},
+    }
+    actions = ["send_message"]
+
+    @admin.action(description="Sends a message to contacts in the selected groups")
+    def send_message(self, request, queryset):
+        if "apply" in request.POST:  # or 'action' in request.POST:
+            message_text = request.POST.get("message").strip()
+            if message_text:
+                recipients = []
+                for group in queryset:
+                    for contact in group.members.all():
+                        recipients.append(contact)
+                msg = send_message(request.user, settings.SVEVE_SENDER_TEXT, message_text, list(set(recipients)))
+                if msg.status == "sent":
+                    self.message_user(request, "Sent message to {} contacts".format(queryset.count()))
+                else:
+                    self.message_user(request, "Failed to send messag: %s" % msg.status_message)
+                return redirect(reverse("admin:sveve_group_changelist"))
+
+        return render(request, "admin/send_message.html", context={"orders": queryset})
 
 
 @admin.register(Message)
